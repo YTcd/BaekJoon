c0299d80ac19b35f4a4b78a014010a9033d39dc0
ytMoon <>
2022-10-28 12:45:33 +0900

    clear - 1271

--- Bronze_V/1271.js
+++ Bronze_V/1271.js
@@ -1,8 +1,8 @@
 let fs = require('fs');
 let input = fs.readFileSync("./input.txt").toString().split(" ");
 
-const a = parseInt(Number(input[0]) / Number(input[1]));
-const b = Number(input[0] % Number(input[1]));
+let a = BigInt(input[0]);
+let b = BigInt(input[1]);
 
-console.log(a);
-console.log(b);
+console.log((a/b).toString());
+console.log((a%b).toString());
--- stdinput.js
+++ stdinput.js
@@ -2,32 +2,14 @@
 // "../input.txt"
 // "/dev/stdin"
 {
+let fs = require('fs');
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+}
+{
     let fs = require('fs');
     let input = fs.readFileSync('./input.txt').toString().split(' ');
-
-    let num = Number(input);
-
-    for (let i = 1; i <= num; i++) {
-        console.log(i);
-    }
 }
 {
     let fs = require('fs');
     let input = fs.readFileSync('./input.txt').toString().split('\n');
-
-    let count = input[0];
-    let numbers = [];
-
-    for (let i = 1; i < input.length; i++) {
-        if (input[i] !== '') {
-            numbers.push(input[i].split(' '));
-        }
-    }
-
-    for (let i = 0; i < numbers.length; i++) {
-        let num1 = Number(numbers[i][0]);
-        let num2 = Number(numbers[i][1]);
-
-        console.log(num1 + num2);
-    }
 }
