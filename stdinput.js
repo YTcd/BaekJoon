033270f946ed9525535493e9889b55a8f8ba759f
ytMoon <lovemytae@naver.com>
2022-10-28 12:55:25 +0900

    clear - 2338

--- stdinput.js
+++ stdinput.js
@@ -7,6 +7,10 @@ let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 }
 {
     let fs = require('fs');
+    let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+    }
+{
+    let fs = require('fs');
     let input = fs.readFileSync('./input.txt').toString().split(' ');
 }
 {
