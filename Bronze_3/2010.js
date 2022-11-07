94b2085c4bb234f9855761e907c7ee0acfb4f11c
ytMoon <lovemytae@naver.com>
2022-11-07 19:42:36 +0900

    clear - 2010

--- Bronze_3/2010.js
+++ Bronze_3/2010.js
@@ -1,9 +1,8 @@
 let fs = require('fs');
-let input = fs.readFileSync("./input.txt").toString().split("\n");
-
-let sum = 0;
+let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+console.log(input);
+let sum = 1;
 for (let i = 1; i < input.length; i++) {
-    input[i] -= 1;
-    sum += input[i];
+    sum += parseInt(input[i]);
 }
-console.log(sum - 1);
+console.log(sum - input[0]);
