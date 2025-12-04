fe479b05013cd927fc0a1a44e35b5743b0e91f95
ytMoon <lovemytae@naver.com>
2025-12-04 19:29:34 +0900

    clear - 11050

--- Bronze/Bronze_1/11050/11050.cpp
+++ Bronze/Bronze_1/11050/11050.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
+int fac(int num)
+{
+    if (num <= 1)
+    {
+        return 1;
+    }
+    return num * fac(num - 1);
+}
+
 int main()
 {
     int N, K;
     cin >> N >> K;
 
-    
+    int result = fac(N) / (fac(K) * fac(N - K));
+    cout << result << endl;
     return 0;
 }
