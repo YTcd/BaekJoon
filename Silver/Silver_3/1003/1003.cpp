4d3abf565ed06f38a7e42a3ba378824d5664d7ed
lovemytae@naver.com <lovemytae@naver.com>
2025-12-17 19:05:50 +0900

    clear - 1003

---
+++ Silver/Silver_3/1003/1003.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> zero(41), one(41);
+    zero[0] = 1; one[0] = 0;
+    zero[1] = 0; one[1] = 1;
+
+    for (int i = 2; i <= 40; i++) {
+        zero[i] = zero[i-1] + zero[i-2];
+        one[i]  = one[i-1]  + one[i-2];
+    }
+
+    int T; 
+    cin >> T;
+    while (T--) {
+        int n; cin >> n;
+        cout << zero[n] << " " << one[n] << "\n";
+    }
+}
