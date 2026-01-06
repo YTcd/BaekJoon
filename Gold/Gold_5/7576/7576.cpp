b175bf92afd7c1596349aed0205f10398ceee2e8
lovemytae@naver.com <lovemytae@naver.com>
2026-01-06 12:48:48 +0900

    clear - 7576

--- Gold/Gold_5/7576/7576.cpp
+++ Gold/Gold_5/7576/7576.cpp
@@ -21,7 +21,7 @@ int main()
 
     vector<vector<int>> vec(row, vector<int>(col));
 
-    vector<cell> cells;
+    queue<cell> cells;
 
     bool isAllDone = true;
     for (int i = 0; i < row; i++)
@@ -37,7 +37,7 @@ int main()
             }
             else if (temp == 1)
             {
-                cells.push_back({i, j, 1});
+                cells.push({i, j, temp});
             }
         }
     }
@@ -48,7 +48,73 @@ int main()
         return 0;
     }
 
-    
+    int count = 0;
+    while(true)
+    {
+        int size = cells.size();
+        for (int i = 0; i < size; i++)
+        {
+            cell temp = cells.front();
+            cells.pop();
+            int x = temp.x;
+            int y = temp.y;
+            int val = temp.val;
+
+            if (x > -1 && x < row && y > -1 && y < col)
+            {
+                if (x+1 < row && vec[x+1][y] == 0)
+                {
+                    vec[x+1][y] = 1;
+                    cells.push({x+1, y, 1});
+                }
+
+                if (x-1 > -1 && vec[x-1][y] == 0)
+                {
+                    vec[x-1][y] = 1;
+                    cells.push({x-1, y, 1});
+                }
+
+                if (y+1 < col && vec[x][y+1] == 0)
+                {
+                    vec[x][y+1] = 1;
+                    cells.push({x, y+1, 1});
+                }
+
+                if (y-1 > -1 && vec[x][y-1] == 0)
+                {
+                    vec[x][y-1] = 1;
+                    cells.push({x, y-1, 1});
+                }
+            }
+        }
+        size = cells.size();
+        if (size == 0)
+        {
+            break;
+        }
+        count++;
+    }
+
+    bool isDone = true;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (vec[i][j] == 0)
+            {
+                isDone = false;
+            }
+        }
+    }
+
+    if (isDone)
+    {
+        cout << count;
+    }
+    else
+    {
+        cout << -1;
+    }
 
     return 0;
 }
