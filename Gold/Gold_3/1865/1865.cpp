8ae2313778a0ca1e2592466f374f17e8bad2678e
lovemytae@naver.com <lovemytae@naver.com>
2026-03-31 14:28:54 +0900

    clear - 1865

--- Gold/Gold_3/1865/1865.cpp
+++ Gold/Gold_3/1865/1865.cpp
@@ -6,26 +6,31 @@ using namespace std;
 
 vector<string> vec;
 
-bool BellmanFord(int startPos, int nodeCnt, vector<vector<pair<int,int>>> &nodes)
+bool BellmanFord(int nodeCnt, vector<tuple<int,int,int>> &edges)
 {
-    vector<int> distance(nodeCnt, INT_MAX);
-    distance[startPos] = 0;
+    vector<int> dist(nodeCnt, 0); // just for minus circular check
 
-    for (int i = 0; i < nodeCnt; i++)
+    for (int i = 0; i < nodeCnt - 1; i++)
     {
-        int linkSize = nodes[i].size();
-        for (int j = 0; j < linkSize; j++)
+        for (auto [u, v, cost] : edges)
         {
-            int endPos = nodes[i][j].first;
-            int cost = nodes[i][j].second;
-            if (distance[i] != INT_MAX && distance[i] + cost < distance[endPos])
+            if (dist[u] + cost < dist[v])
             {
-                distance[endPos] = distance[i] + cost;    
+                dist[v] = dist[u] + cost;
             }
         }
     }
 
-    distance;
+    // check minus circular
+    for (auto [u, v, cost] : edges)
+    {
+        if (dist[u] + cost < dist[v])
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void solveTestCase()
@@ -33,31 +38,29 @@ void solveTestCase()
     int nodeCnt, lineCnt, holeCnt;
     cin >> nodeCnt >> lineCnt >> holeCnt;
 
-    vector<vector<pair<int,int>>> nodes(nodeCnt);
+    vector<tuple<int,int,int>> edges;
 
-    for (int i = 0; i < lineCnt + holeCnt; i++)
+    // edges
+    for (int i = 0; i < lineCnt; i++)
     {
         int startPos, endPos, cost;
         cin >> startPos >> endPos >> cost;
 
-        if (i < lineCnt)
-        {
-            nodes[startPos-1].push_back({endPos-1, cost});
-        }
-        else
-        {
-            nodes[startPos-1].push_back({endPos-1, -cost});
-        }
-
-        // nodes[시작점 - 1] == {(끝점 - 1), 코스트}
+        edges.push_back({startPos - 1, endPos - 1, cost});
+        edges.push_back({endPos - 1, startPos - 1, cost});
     }
-    
 
-    for (int i = 0; i < nodeCnt; i++)
+    // wormholes
+    for (int i = 0; i < holeCnt; i++)
     {
-        bool result = BellmanFord(i, nodeCnt, nodes);
-        vec.push_back(result ? "YES" : "NO");        
+        int startPos, endPos, cost;
+        cin >> startPos >> endPos >> cost;
+
+        edges.push_back({startPos - 1, endPos - 1, -cost});
     }
+
+    bool result = BellmanFord(nodeCnt, edges);
+    vec.push_back(result ? "YES" : "NO");
 }
 
 int main()
@@ -65,15 +68,17 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    short testCaseCnt;
+    int testCaseCnt;
     cin >> testCaseCnt;
+
+    int originalCnt = testCaseCnt;
 
     while (testCaseCnt--)
     {
         solveTestCase();
     }
 
-    for (int i = 0; i < testCaseCnt; i++)
+    for (int i = 0; i < originalCnt; i++)
     {
         cout << vec[i] << "\n";
     }
