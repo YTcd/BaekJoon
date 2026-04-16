51722508d2ebe4020b101f0b2b29158b607de9f1
moonyt_home <lovemytae@naver.com>
2026-04-17 01:36:58 +0900

    clear - 2166

--- Gold5/2166/2166.cpp
+++ Gold5/2166/2166.cpp
@@ -9,5 +9,32 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int cnt;
+    cin >> cnt;
+
+    vector<pair<long long, long long>> arr(cnt);
+    for (int i = 0; i < cnt; i++)
+    {
+        cin >> arr[i].first >> arr[i].second;
+    }
+
+    long long sum = 0;
+    for (int i = 0; i < cnt; i++)
+    {
+        int j = (i + 1) % cnt;
+        sum += (arr[i].first * arr[j].second) - (arr[i].second * arr[j].first);
+    }
+
+    long long result = abs(sum) / 2;
+    // 소수점이하 1자리까지 출력
+    if (sum % 2 == 0)
+    {
+        cout << result << ".0" << endl;
+    }
+    else
+    {
+        cout << result << ".5" << endl;
+    }
+
     return 0;
 }
